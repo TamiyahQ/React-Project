00465519bfa5e45cfeedf54f7530d17fe523a098
Tamiyah <tamiyahqazi356@gmail.com>
2024-07-29 21:12:30 -0700

    changed

--- src/components/Header/Contact.jsx
+++ src/components/Header/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       </div>
     <div className="container-fluid " style={{backgroundColor:"white"}}>
     <div className="row d-flex justify-content-center align-items-center ">
-      <div className="col-12 col-sm-4 col-md-5 col-lg-8 d-flex justify-content-center align-items-center pt-4">
+      <div className="col-12 col-sm-5 col-md-5 col-lg-8 d-flex justify-content-center align-items-center pt-4">
       <h1><b>Contact Us</b></h1>     
       </div>
       <div  className="col-12 col-sm-4 col-md-5 col-lg-8 d-flex text-container justify-content-center align-items-center my-3 mx-4">
@@ -20,10 +20,10 @@ const Contact = () => {
        <label className="p-3 pe-5 fw-bold">Email</label>
        <input className="custom-input p-2 px-3" type="email" placeholder="Enter your valid email" />
        </div>
-        <div className="p-3">
-        <label className="p-3 pe-4  fw-bold">Message</label>
-        <input className="custom-input p-2 px-3" type="text" placeholder="Type your message" />
-        </div>
+       <div className="p-3 d-flex align-items-start">
+       <label className="fw-bold me-4 ps-3">Message</label>
+       <textarea className="custom-input p-2 px-3" placeholder="Type your message"></textarea>
+       </div>
         <div  className="col-12 col-sm-10 col-md-10 col-lg-10 d-flex text-container justify-content-right align-items-center my-3 mx-4">
         <button type="button" class="btn btn-primary" style={{backgroundColor:"rgba(76, 175, 79, 1)", borderColor:"rgba(76, 175, 79, 1)"}}>Submit</button>
         </div>
